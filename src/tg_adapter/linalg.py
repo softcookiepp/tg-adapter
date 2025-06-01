7427c711924dcfdc41fb35b42737b7a8e82f1840
softcookiepp <softcookiepp@protonmail.com>
2025-06-01 16:20:05 -0700

    working on eig

--- src/tg_adapter/linalg.py
+++ src/tg_adapter/linalg.py
@@ -2,7 +2,7 @@ import tinygrad
 from .utils import is_jitted
 import numpy as np
 from .lambdas import diag, check_2d
-
+from .tensor import convert_to_torch, convert_to_tg
 
 
 def norm(A, ord = None, dim = None, keepdim = False, out = None, dtype = None):
@@ -13,8 +13,13 @@ def svd(A, full_matrices = True, driver = None, out = None):
 	A = A.tg
 	ATA = A.T @ A
 	
+def qr(*args, **kwargs):
+	raise NotImplemented
+	
 def eig(A, max_iter = 100, tol = 1e-6, out = None):
-	raise NotImplementedError
+	# gotta see if jit is being used
+	using_jit = is_jitted()
+	
 	A = A.tg
 	n = A.shape[0]
 	V = tinygrad.Tensor.eye(n, dtype = A.dtype, device = A.device)
@@ -22,5 +27,11 @@ def eig(A, max_iter = 100, tol = 1e-6, out = None):
 		Q, R = qr(A)
 		A = R @ Q
 		V = V @ Q
-		#off_diag = (A - ).abs()
+		off_diag = (A - diag(diag(A)) ).abs()
+		if (not using_jit) and off_diag.max() < tol:
+			# can't check booleans when jit is being used
+			break
 	
+	eigenvalues = diag(A)
+	return convert_to_torch(eigenvalues, V)
+
