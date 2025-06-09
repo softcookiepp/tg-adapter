34cc50406426ad9dec98d89e0621972564fcd720
softcookiepp <softcookiepp@protonmail.com>
2025-06-09 09:09:43 -0700

    well, we figured out appending!

--- src/tg_adapter/nn/module_list.py
+++ src/tg_adapter/nn/module_list.py
@@ -36,7 +36,10 @@ class ModuleList(Module):
 
 	def __init__(self, modules: Optional[Iterable[Module]] = None):
 		super().__init__()
-		self._modules = list(modules)
+		# _modules is not assignable, so we actually have to append it internally...
+		for m in modules:
+			self.append(m)
+		
 
 	def _get_abs_string_index(self, idx):
 		"""Get the absolute index for the list of modules."""
