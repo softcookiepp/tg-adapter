b503858267432e9dd0e5e43c4fa99591f2d7b9f1
softcookiepp <softcookiepp@protonmail.com>
2025-06-16 10:28:25 -0700

    oopsie

--- src/tg_adapter/types.py
+++ src/tg_adapter/types.py
@@ -101,7 +101,7 @@ NP_TG_TYPE_MAP = {
 def get_np_type_from_torch(tt, backend = "DEFAULT"):
 	if not isinstance(tt, str):
 		tt = tt.key
-	return NP_TG_TYPE_MAP[ TG_BACKEND_TYPE_MAP[tt] ]
+	return NP_TG_TYPE_MAP[ TG_BACKEND_TYPE_MAP[backend][tt] ]
 
 def get_np_type_from_tg(tgt):
 	return NP_TG_TYPE_MAP[tgt]
