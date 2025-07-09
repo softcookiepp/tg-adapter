c48f4fa48c6ec72a0a229a51c4a9db1dfc27d259
softcookiepp <softcookiepp@protonmail.com>
2025-07-08 22:38:26 -0700

    me forget

--- src/tg_adapter/utils/__init__.py
+++ src/tg_adapter/utils/__init__.py
@@ -2,6 +2,7 @@ from . import checkpoint
 from .tg_helpers import *
 from .grad import no_grad
 from . import hooks
+from . import _pytree
 
 def process_shape(*args):
 	if isinstance(args[0], tuple):
